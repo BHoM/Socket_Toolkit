6b383dd306b5389e2fc8550555befd3cdaef461d
adecler <arnaud.declercq@burohappold.com>
2017-07-24 12:31:09 +0100

    migrate sockets to using TCP instead of UDP

---
+++ Socket_Test/Program.cs
@@ -0,0 +1,83 @@
+﻿using BH.Adapter.Socket;
+using BH.oM.Base;
+using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Socket_Test
+{
+    class A
+    {
+
+        public A(double gVal = 0, double pVal = 0)
+        {
+            getProp = gVal;
+            privateField = pVal;
+        }
+
+        public double a { get; set; }
+        public double publicField;
+        private double privateField;
+        public double getProp { get; private set; }
+    }
+    class B : A { public double b { get; set; } }
+    class C : A { public double c { get; set; } }
+    class D : B { public double d { get; set; } }
+    class E : C { public double e { get; set; } }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestTcpSocket();
+
+            Console.ReadLine();
+        }
+
+
+        static void TestTcpSocket()
+        {
+            List<object> items = new List<object>
+            {
+                new A (-6, -7) { a = 1, publicField = -4 },
+                new B { a = 2, b = 45 },
+                new C { a = 3, c = 56 },
+                new D { a = 4, b = 67, d = 123 },
+                new E { a = 5, c = 78, e = 456 },
+                new Node {Point = new Point(1, 2, 3), Name = "X"},
+                new List<BHoMObject> {
+                    new Node {Point = new Point(1, 2, 3), Name = "A"},
+                    new Node {Point = new Point(4, 5, 6), Name = "B"},
+                    new Node {Point = new Point(7, 8, 9), Name = "C"}
+                },
+                new Dictionary<string, A> {
+                    { "A",  new A { a = 1 } },
+                    { "C",  new C { a = 3, c = 56 } },
+                    { "E",  new E { a = 5, c = 78, e = 456 } }
+                }
+            };
+
+            for (int i = 0; i < 10000; i++)
+                items.Add(new Node { Point = new Point(i, 2, 3), Name = ("X" + i) });
+
+            int port = 8888;
+            SocketServer server = new SocketServer(port);
+            server.DataObservers += ReceivedData;
+
+            Console.WriteLine("Sending data...");
+            SocketLink client = new SocketLink("127.0.0.1", port);
+            client.SendData(items);
+        }
+
+        static void ReceivedData(List<object> objects)
+        {
+            Console.WriteLine("Received " + objects.Count + " objects");
+        }
+    }
+}
