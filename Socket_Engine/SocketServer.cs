9e0441bf94aa25b9d706ef887ef610334ef50041
adecler <arnaud.declercq@burohappold.com>
2017-12-31 10:23:03 +0800

    Removing unnecessary usings

--- Socket_Engine/SocketServer.cs
+++ Socket_Engine/SocketServer.cs
@@ -1,15 +1,9 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.IO;
-using MongoDB.Bson.Serialization;
+﻿using MongoDB.Bson.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
 
 namespace BH.Adapter.Socket
 {
