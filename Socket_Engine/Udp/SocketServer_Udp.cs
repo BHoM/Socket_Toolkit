e48e6f2b17a1028aead5bf97df0eec3d24b4bca5
adecler <arnaud.declercq@burohappold.com>
2017-12-31 10:23:03 +0800

    Removing unnecessary usings

--- Socket_Engine/Udp/SocketServer_Udp.cs
+++ Socket_Engine/Udp/SocketServer_Udp.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using SS = System.Net.Sockets;
 using System.Threading;
--- Socket_Test/Program.cs
+++ Socket_Test/Program.cs
@@ -4,10 +4,7 @@ using BH.oM.Geometry;
 using BH.oM.Structural.Elements;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Socket_Test
 {
